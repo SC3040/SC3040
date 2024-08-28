328b25506994463e1a3fb38511d8979779353551
Lim Jia Earn <119128612+LimJiaEarn@users.noreply.github.com>
2024-08-28 22:22:17 +0800

    env var for docker frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Home</p>
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <p>Home</p>
+          <code className="bg-gray-200 p-2 rounded">
+              {process.env.NEXT_PUBLIC_RUNTIME_BACKEND_URL || 'Not set'}
+          </code>
+      </main>
   );
 }
