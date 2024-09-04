aa0b911ddce7b30900ef70c551bc00a56c6f7bb2
Lim Jia Earn <119128612+LimJiaEarn@users.noreply.github.com>
2024-09-04 13:54:16 +0800

    fixed docker compose structure

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.watchOptions = {
-                poll: 1000,
-                aggregateTimeout: 300,
-            }
-        }
-        return config
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
