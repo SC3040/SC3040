6bfdd3dc70c43e71a84dc61f70cab130b9575068
Adrian Ong <ongadrian98@gmail.com>
2024-09-09 20:09:33 +0800

    Added data validation to receipt: total_cost, date

--- microservices/receiptService/Receipt.py
+++ microservices/receiptService/Receipt.py
@@ -1,8 +1,11 @@
 from enum import Enum
 from typing import List
+from dateutil import parser
+from price_parser import Price
 
 
 class ReceiptError(Exception):
+    """To format an error message to reply to LLM"""
     def __init__(self, field_name_with_error: str, error_msg: str):
         self.field_name = field_name_with_error
         self.error_msg = error_msg
@@ -36,16 +39,55 @@ class Item:
 
 
 class Receipt:
-    def __init__(self, shop_name: str, date: str, total_cost: str, category: Category, itemized_list: List[Item]):
-        self.shop_name = shop_name
-        self.date = date
-        self.total_cost = total_cost
-        self.category = Category.validate_category(category)
-        self.itemized_list = itemized_list
+    def __init__(self, merchant_name: str, date: str, total_cost: str, category: Category, itemized_list: List[Item]):
+        self.merchant_name = merchant_name
+        self._date = self.parse_date(date)
+        self._total_cost = self.parse_total_cost(total_cost)
+        self._category = Category.validate_category(category)
+        self._itemized_list = self.parse_itemized_list(itemized_list)
 
-    def validate_input(self):
+        # Additional validation
         self.validate_non_empty()
-        # Can add more validation if needed
+
+    @property
+    def date(self) -> str:
+        return self._date.strftime("%d/%m/%Y")
+
+    @property
+    def category(self) -> str:
+        return str(self._category.name)
+
+    @property
+    def total_cost(self) -> str:
+        return str(self._total_cost.amount)
+
+    @property
+    def itemized_list(self) -> List[Item]:
+        return [Item(item['item_name'], item['item_quantity'], str(item['item_cost'].amount)) for item in self._itemized_list]
+
+    @staticmethod
+    def parse_date(date_string: str):
+        try:
+            return parser.parse(date_string)
+        except ValueError:
+            raise ReceiptError("date", f"Invalid date format: '{date_string}'. If possible, provide a valid date in the format: 'YYYY-MM-DD'")
+
+    @staticmethod
+    def parse_total_cost(total_cost: str):
+        price_obj = Price.fromstring(total_cost)
+
+        if price_obj.amount is None:
+            raise ReceiptError("total_cost", f"Invalid total cost '{total_cost}'. Please provide a valid number")
+
+        return price_obj
+
+    @staticmethod
+    def parse_itemized_list(itemized_list: List[Item]):
+        # Loop each item and parse the cost
+        for item_dict in itemized_list:
+            item_dict['item_cost'] = Receipt.parse_total_cost(item_dict['item_cost'])
+
+        return itemized_list
 
     def validate_non_empty(self):
         for attr, value in self.__dict__.items():
