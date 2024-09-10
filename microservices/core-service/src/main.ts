e0f4b7cb47e75526606f8f71e90e92e7bbeb3104
Alexlaaa <leealessandro.sg@gmail.com>
2024-09-10 08:55:34 +0800

    chore: init project skeleton structure

---
+++ microservices/core-service/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('ExpenseNote')
+    .setDescription('An easy way to manage and track your personal finances')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+
+  await app.listen(8080);
+}
+bootstrap();
