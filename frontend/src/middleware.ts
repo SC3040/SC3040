9179d18183355afce737b9c78dd7ce78daa85274
J <94799574+jpl12345@users.noreply.github.com>
2024-10-22 19:37:36 +0800

    Implement fix for FE homepage imgs

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,18 +1,13 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 import { PUBLIC_PATHS } from "@/constants"
- 
+
 export function middleware(request: NextRequest) {
-
-  // const allCookies = request.cookies.getAll();
-  // console.log(`[Middleware] All cookies:`, allCookies);
-
   const jwt = request.cookies.get('jwt');
   console.log(`[Middleware] JWT cookie:`, jwt);
 
-  const path : string = request.nextUrl.pathname
+  const path: string = request.nextUrl.pathname;
 
-  // Handle all requests, including API and server action requests
   if (path.startsWith('/api/') || path.includes('/route/')) {
     console.log("[Middleware] Handling API or server action request");
     if (!jwt) {
@@ -22,7 +17,6 @@ export function middleware(request: NextRequest) {
         { status: 401, headers: { 'Content-Type': 'application/json' } }
       );
     }
-    // If JWT exists, clone the request and add the Authorization header
     const newHeaders = new Headers(request.headers);
     newHeaders.set('Authorization', `Bearer ${jwt.value}`);
     return NextResponse.next({
@@ -31,22 +25,20 @@ export function middleware(request: NextRequest) {
       },
     });
   }
-  
-  // redirect unauthenticated users to sign in page if they try to access protected pages
+
   if (jwt === undefined && !isPublicPath(path)) {
-      const prevPath : string = encodeURIComponent(path)
-      return NextResponse.redirect(new URL(`/signin?from=${prevPath}`, request.url))
+    const prevPath: string = encodeURIComponent(path);
+    return NextResponse.redirect(new URL(`/signin?from=${prevPath}`, request.url));
   }
 
-  // redirect authenticated users entering public path to home page
-  // Exclude '/home' from this check
+
   if (jwt !== undefined && isPublicPath(path) && path !== '/home') {
-      return NextResponse.redirect(new URL('/home', request.url))
+    return NextResponse.redirect(new URL('/home', request.url));
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
- 
+
 export const config = {
   matcher: [
     /*
@@ -55,12 +47,14 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - images (static image files)
+     * - logos
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|images/|logos/).*)',
   ],
 }
 
-const publicPaths: Set<string> = new Set(PUBLIC_PATHS)
+const publicPaths: Set<string> = new Set(PUBLIC_PATHS);
 function isPublicPath(path: string): boolean {
-    return publicPaths.has(path)
+  return publicPaths.has(path);
 }
